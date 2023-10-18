ce38ae0cc1be83a5ef9e957b91cd22d9aa759067
Praneel04 <117033281+Praneel04@users.noreply.github.com>
2023-10-18 19:33:04 +0530

    Add files via upload

---
+++ chessmain.py
@@ -0,0 +1,107 @@
+'''Current game state and user input'''
+import pygame as p
+from Chess import chesseng
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8  # dimension pf chess
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 12  # for animations later on
+IMAGES = {}
+'''
+Initialize a global dictionary of images 
+'''
+
+
+def load_Images():
+    pieces = {'wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ'}
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = chesseng.Gamestate()
+    load_Images()
+    running = True
+    sq_selected = ()
+    playerClicks = []  # keep track of player clicks
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x, y) location of the mouse
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+
+                if sq_selected == (row, col):
+                    sq_selected = ()
+                    playerClicks = []  # if already selected, then clear
+                else:
+                    sq_selected = (row, col)
+                    playerClicks.append(sq_selected)  # append for first and second clicks
+
+                if len(playerClicks) == 2:
+                    Move = chesseng.move(playerClicks[0], playerClicks[1], gs.board, gs.whiteToMove, gs)
+
+                    # print(gs.moveLog)
+
+
+                    if Move.isValid(gs.board):
+
+                        gs.moveLog.append(Move.getChessNotation())
+
+
+
+
+                        gs.makeMove(Move)
+                        print(gs.moveLog)
+                        print(Move.getChessNotation())
+                        print(gs.bestmove(gs.whiteToMove))
+                        if(gs.is_checkmate()):
+                            print("Its Checkmate!")
+                            running=False
+                        if(gs.is_stalemate()):
+                            print("Its Stalemate!")
+                            running=False
+                    else:
+                        sq_selected = ()
+                        playerClicks = []
+
+                    sq_selected = ()
+                    playerClicks = []
+
+        drawGameState(screen, gs)
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("brown")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c];
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+
+if __name__ == "__main__":
+    main()
